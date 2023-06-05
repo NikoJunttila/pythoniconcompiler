ee9eddd96be427510b9a81cbf2c2cb9178b215e5
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2023-06-05 14:07:04 +0300

    bug fix

--- copyfiles.py
+++ copyfiles.py
@@ -1,7 +1,7 @@
 import shutil
 import os
 import tkinter as tk
-from tkinter import filedialog, Listbox, Scrollbar, MULTIPLE, Label, PhotoImage, Scrollbar
+from tkinter import filedialog, Listbox, Scrollbar, MULTIPLE, Label, PhotoImage, Scrollbar, Button, Entry
 from PIL import Image, ImageTk
 
 
@@ -9,7 +9,7 @@ def get_svg_files(folder_path, search_term=None):
     svg_files = []
     for root, _, files in os.walk(folder_path):
         for file in files:
-            if file.endswith((".svg", ".png", ".jpg", ".jpeg", ".theme")):
+            if file.endswith((".svg", ".png", ".jpg", ".jpeg")):
                 if search_term is None or search_term.lower() in file.lower():
                     svg_files.append(os.path.join(root, file))
     return svg_files
@@ -58,6 +58,9 @@ def copy_files():
             shutil.copy(file, destination_path)
 
         result_label.config(text="Files copied successfully.")
+        update_added_icons_list(destination_folder)
+        listbox.selection_clear(0, tk.END)
+        selected_listbox.delete(0, tk.END)  # Empty the selected listbox
 
     except Exception as e:
         result_label.config(text="An error occurred: " + str(e))
@@ -115,6 +118,9 @@ def create_index_theme():
         theme_label.config(text="An error occurred: " + str(e))
 
 
+selected_files = []  # Global list variable to store the selected indices
+
+
 def search_files():
     search_term = search_entry.get()
     source_folder = source_entry.get()
@@ -128,6 +134,10 @@ def search_files():
     for file in svg_files:
         listbox.insert(tk.END, file)
 
+    # Restore the previous selection
+    for index in selected_files:
+        listbox.select_set(index)
+
 def browse_source_folder():
     folder_path = filedialog.askdirectory()
     source_entry.delete(0, tk.END)
@@ -138,6 +148,7 @@ def browse_destination_folder():
     folder_path = filedialog.askdirectory()
     destination_entry.delete(0, tk.END)
     destination_entry.insert(0, folder_path)
+    update_added_icons_list(folder_path)
 
 def update_svg_list(folder_path):
     svg_files = get_svg_files(folder_path)
@@ -146,15 +157,31 @@ def update_svg_list(folder_path):
         listbox.insert(tk.END, file)
 
 def update_selected_list():
+    global selected_files
     selected_files = listbox.curselection()
+
     selected_listbox.delete(0, tk.END)
     for index in selected_files:
         selected_listbox.insert(tk.END, listbox.get(index))
+    show_image()
+
+def update_added_icons_list(folder_path):
+    added_icons_listbox.delete(0, tk.END)
+    added_icons = get_svg_files(folder_path)
+    for icon in added_icons:
+        added_icons_listbox.insert(tk.END, icon)
+
+def change_resolution(image_path, new_width, new_height, output_path):
+    image = Image.open(image_path)
+    # Resize the image
+    resized_image = image.resize((new_width, new_height))
+    # Save the resized image
+    resized_image.save(output_path)
 
 def show_image():
     selection = listbox.curselection()
     if selection:
-        index = int(selection[0])
+        index = int(selection[len(selection) - 1])
         image_path = listbox.get(index)
         image = Image.open(image_path)
         image.thumbnail((300, 300))  # Resize the image if necessary
@@ -162,11 +189,32 @@ def show_image():
         image_label.config(image=photo)
         image_label.image = photo  # Store reference to avoid garbage collection
 
+        width, height = image.size
+        dimensions_label.config(text=f"Dimensions: {width}x{height}")
+
+def resize_image():
+    # Get the selected image path
+    selection = listbox.curselection()
+    index = int(selection[len(selection) - 1])
+    image_path = listbox.get(index)
+
+    # Get the desired resolution
+    new_width = int(width_entry.get())
+    new_height = int(height_entry.get())
+
+    # Open a file dialog to select a save location for the resized image
+    output_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=(("JPEG File", "*.jpg"), ("PNG File", "*.png"), ("All Files", "*.*")))
+    # Change the resolution of the image and save the resized image
+    change_resolution(image_path, new_width, new_height, output_path)
+    result_label.config(text="Image Resized and Saved!")
+
+def ignore_click(event):
+    return "break"
 
 # Create the GUI window
 window = tk.Tk()
 window.title("ICON File Copy")
-window.geometry("600x600")
+window.geometry("900x900")
 
 # Create labels and entry fields
 source_label = tk.Label(window, text="Source Folder:")
@@ -225,14 +273,14 @@ listbox_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 listbox_scrollbar = Scrollbar(listbox_frame, orient=tk.VERTICAL)
 listbox_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-listbox = Listbox(listbox_frame, selectmode=MULTIPLE)
+listbox = Listbox(listbox_frame, selectmode=MULTIPLE,font=("Helvetica",9))
 listbox.pack(fill=tk.BOTH, expand=True)
 listbox.config(yscrollcommand=listbox_scrollbar.set)
 listbox_scrollbar.config(command=listbox.yview)
 
 # Create listbox to display selected SVG files
 selected_frame = tk.Frame(window)
-selected_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+selected_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 selected_listbox_scrollbar = Scrollbar(selected_frame, orient=tk.VERTICAL)
 selected_listbox_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
@@ -241,6 +289,20 @@ selected_listbox = Listbox(selected_frame)
 selected_listbox.pack(fill=tk.BOTH, expand=True)
 selected_listbox.config(yscrollcommand=selected_listbox_scrollbar.set)
 selected_listbox_scrollbar.config(command=selected_listbox.yview)
+selected_listbox.bind("<Button-1>", ignore_click)
+
+# destination folder list update_added_icons_list(destination_folder)
+added_icons_frame = tk.Frame(window)
+added_icons_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+added_icons_scrollbar = Scrollbar(added_icons_frame, orient=tk.VERTICAL)
+added_icons_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+added_icons_listbox = Listbox(added_icons_frame)
+added_icons_listbox.pack(fill=tk.BOTH, expand=True)
+added_icons_listbox.config(yscrollcommand=added_icons_scrollbar.set)
+added_icons_scrollbar.config(command=added_icons_listbox.yview)
+
 
 # Create button to copy files
 copy_button = tk.Button(window, text="Copy Files", command=copy_files)
@@ -257,11 +319,26 @@ listbox.bind("<<ListboxSelect>>", lambda event: update_selected_list())
 
 
 # Create image label
-show_button = tk.Button(window, text="Show Image", command=show_image)
-show_button.pack()
+#show_button = tk.Button(window, text="Show Image", command=show_image)
+#show_button.pack()
 
 image_label = Label(window)
 image_label.pack()
 
-# Run the GUI
+dimensions_label = Label(window, text="current dimensions")
+dimensions_label.pack()
+
+width_label = Label(window, text="New Width:")
+width_label.pack()
+width_entry = Entry(window)
+width_entry.pack()
+
+height_label = Label(window, text="New Height:")
+height_label.pack()
+height_entry = Entry(window)
+height_entry.pack()
+
+resize_button = Button(window, text="Resize Image", command=resize_image)
+resize_button.pack()
+
 window.mainloop()
